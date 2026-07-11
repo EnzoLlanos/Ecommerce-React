5f22c21b37b31e3d654681074c73cea65520cb28
Aggs13 <agusgualda033@gmail.com>
2026-07-11 19:37:33 -0300

    Add NavBar component and restructure layout
    Created a new NavBar component with a toggle hamburger menu for navigation. Refactored App.jsx to use a flex-based layout with the navbar on the left and main content on the right. Added corresponding styles and implemented the Profile component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 import ProductsList from "./pages/Products/ProductsList/ProductsList";
 import ProductView from "./pages/Products/ProductView/ProductView";
 import NewProduct from "./pages/Products/NewProduct/NewProduct";
 import Profile from "./pages/Profile/Profile";
 import NotFound from "./pages/Errors/NotFound/NotFound";
-;
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element= {<Home />}/>
-      <Route path="/products" element= {<ProductsList/>}/>
-      <Route path= "/products/new" element= {<NewProduct/>}/>
-      <Route path= "/Products/:id" element= {<ProductView/>}/>
-      <Route path= "/profile" element= {<Profile/>}/>
-      <Route path= "*" element= {<NotFound/>}/>
+    <div className='layout'>
 
-    </Routes>
+      <NavBar></NavBar>
+
+
+      <main className='contenido'>
+        <Routes>
+          <Route path="/" element= {<Home />}/>
+          <Route path="/products" element= {<ProductsList/>}/>
+          <Route path= "/products/new" element= {<NewProduct/>}/>
+          <Route path= "/Products/:id" element= {<ProductView/>}/>
+          <Route path= "/profile" element= {<Profile/>}/>
+          <Route path= "*" element= {<NotFound/>}/>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import { useState } from "react"
+import "./NavBarStyle.css"
+
+function NavBar(){
+  const [menu,setMenu] = useState(false)
+  return(
+
+    <div className="NavBar">
+      <button onClick={() => setMenu(!menu)}>{menu ? "|  X  |" : "|  ☰  |"}</button>
+      {menu && (
+
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/products">Products</Link></li>
+          <li><Link to="/profile">profile</Link></li>
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default NavBar
