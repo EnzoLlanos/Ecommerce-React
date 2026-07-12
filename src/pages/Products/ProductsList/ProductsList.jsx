e467af752d42099c76214529e76c1142db88a425
Aggs13 <agusgualda033@gmail.com>
2026-07-12 17:25:03 -0300

    Update ProductsList.jsx

--- src/pages/Products/ProductsList/ProductsList.jsx
+++ src/pages/Products/ProductsList/ProductsList.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ProductsList.css';
 
 function ProductsList() {
+  const [products, setProducts] = useState([])
+  const getProducts = async() => {
+    const resp = await fetch("http://localhost:3000/api/productos/",{
+      method : "GET",
+      headers: {
+      "Content-Type": "application/json",
+      }
+    })
+    const data = await resp.json();
+    setProducts(data.data);
+    console.log(data.data)
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  
+
   return (
     <div className="products-list">
       <h1>Products List</h1>
+      {products.map((p) => (
+        <div>
+
+          <p>{p.nombre}</p>
+          <p>${p.precio}</p>
+          <p></p>
+        </div>
+      ))}
     </div>
   );
 }
