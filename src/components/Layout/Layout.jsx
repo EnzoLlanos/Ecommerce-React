735b45e92a2a0a4b1dde6ea891d88c368af0d5e8
EnzoLlanos <enzollanos16@gmail.com>
2026-07-12 20:28:48 -0300

    Click
    ahora al clickear fuera del NavBar se cierra

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
 
 function Layout({ children }) {
   const [menuAbierto, setMenuAbierto] = useState(true);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const click = (e) =>{
+        if(navRef.current && !navRef.current.contains(e.target) && menuAbierto){
+            setMenuAbierto(false)
+        }
+    }
+  
+
+  document.addEventListener("mousedown", click)
+    return () => document.removeEventListener("mousedown", click)
+  }, [menuAbierto])
 
   return (
     <div className="app-layout">
-      <NavBar
-        menuAbierto={menuAbierto}
-        setMenuAbierto={setMenuAbierto}
-      />
-
+        <div ref={navRef}>
+            <NavBar menuAbierto={menuAbierto} setMenuAbierto={setMenuAbierto}/>
+        </div>
       <main className="app-content">
         {children}
       </main>
