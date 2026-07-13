06c6105285d66426a9d80adffe5bcb4e982f9a71
EnzoLlanos <enzollanos16@gmail.com>
2026-07-13 14:29:43 -0300

    Estilos
    le agregue detalles al los estilos ademas de un nombre al navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,43 @@
 import { NavLink } from "react-router-dom"
 import "./NavBarStyle.css"
+import {FaHome, FaBoxOpen, FaUser} from "react-icons/fa"
 
 function NavBar({menuAbierto, setMenuAbierto}) {
   return(
-    <>
+    
     <aside className={`NavBar ${menuAbierto ? "open" : "closed"}`}>
+      <div className="NavBar-header">
+        <span className="NavBar-logo">Mi Ecommerce</span>
+        <button className="menu-toggle" onClick={() => setMenuAbierto(!menuAbierto)} >
+        {menuAbierto ? "✕" : "☰"}
+      </button>
+      </div>
+
+      
         <ul>
-          <li><NavLink end to="/">Home</NavLink></li>
-          <li><NavLink to="/products">Products</NavLink></li>
-          <li><NavLink to="/profile">profile</NavLink></li>
+          <li>
+            <NavLink end to="/">
+              <FaHome className="icon"/>
+              <span className="text">Home</span>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/products">
+              <FaBoxOpen className="icon"/>
+              <span className="text">Products</span>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/profile">
+              <FaUser className="icon"/>
+              <span className="text">profile</span>
+            </NavLink>
+          </li>
         </ul>
     </aside>
-    <button className="menu-toggle" onClick={() => setMenuAbierto(!menuAbierto)} >
-      {menuAbierto ? "✕" : "☰"}
-    </button>
-    </>
+  
   )
 }
 
